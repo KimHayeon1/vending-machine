ea1a2e50bb3b74bed8838973deed2223eddf95bb
Kim Hayeon <k.hayeon00@gmail.com>
2023-04-06 23:51:24 +0900

    Refactor : HTML 불필요한 요소 제거 및 CSS 수정중

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const products = document.getElementsByClassName('product');
+const cartList = document.getElementById('cart');
 
 [...products].forEach(product => {
   product.addEventListener('click', handleProduct);
 });
 
 function handleProduct(event) {
-  if (!event.currentTarget.style.outline) {
-    event.currentTarget.style.outline = "3px solid var(--main-color)";
+  const current = event.currentTarget;
+  const urlImg = current.getElementsByTagName('img')[0].getAttribute('src')
+
+  if (!current.style.outline) {
+    current.style.outline = "3px solid var(--main-color)";
+    createCartItem()
   } else {
-    event.currentTarget.style.outline = "";
+    current.style.outline = "";
   }
+}
+
+function createCartItem() {
+  const li = document.createElement('li')
+  const img = document.createElement('img')
+  const p = document.createElement('p')
+  const input = document.createElement('input')
+  
+  input.setAttribute('value', '1');
+  input.setAttribute('type', 'number');
+  
+  cartList.appendChild(li);
+  li.appendChild(img);
+  li.appendChild(p);
+  li.appendChild(input);
+}
+
+function deleteCartItem() {
 }
